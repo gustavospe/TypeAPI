cbb53e9cc126bd655918ec6772bdc667d5397ffd
Gustavo Spengler <dev.gustavospengler@gmail.com>
2025-07-02 11:37:56 -0300

    Migrations

---
+++ src/database/1751466957936-CreateProduto.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateProduto1751466957936 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -7,12 +7,12 @@ dotenv.config();
 const AppDataSource = new DataSource({
     type: "mysql", // ou "mysql", "sqlite", etc.
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [Produto],
-    synchronize: true, // Sincroniza automaticamente as entidades com o banco
+    synchronize: Boolean(process.env.DB_SYNC), // Sincroniza automaticamente as entidades com o banco
 });
 
 export default AppDataSource;
